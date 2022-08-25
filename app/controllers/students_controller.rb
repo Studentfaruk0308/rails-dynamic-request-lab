630930ffb2a80589dd290d96623f2bf438d6ce86
faruk <studentfaruk0308@gmail.com>
2022-08-25 17:03:47 +1000

    After Completion 25082022

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,5 +1,12 @@
 class StudentsController < ApplicationController
   def index
     @students = Student.all
+
+    render 'show'
   end
+
+  def show
+    @student = Student.find(params[:id])
+  end
+
 end
