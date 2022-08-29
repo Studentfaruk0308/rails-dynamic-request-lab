82962c46c2b43ed7a90c4f9c71fa20a1ca6dfd4b
faruk <studentfaruk0308@gmail.com>
2022-08-29 13:11:32 +1000

    29082022 1311

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -2,7 +2,7 @@ class StudentsController < ApplicationController
   def index
     @students = Student.all
 
-    render 'show'
+    # render 'show'
   end
 
   def show
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+# Student.create(first_name: 'Emanuel', last_name: 'Macron')
+
+Student.create(first_name: "Daenerys", last_name: "Targaryen")
+Student.create(first_name: "Lindsey", last_name: "Stirling")
+
